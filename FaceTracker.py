20ee05315f6da48b5d73ac0524dd15e652c91881
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-27 15:48:01 +0200

    Added fps

--- FaceTracker.py
+++ FaceTracker.py
@@ -4,8 +4,16 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+pTime, cTime = 0
+
 while True:
     success, img = cap.read()
+    
+    cTime = time.time()
+    fps = 1/ (cTime - pTime)
+    pTime = cTime
+    
+    cv2.putText(img, f'fps: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
     
     cv2.imshow("Image", img)
     cv2.waitKey(1)
