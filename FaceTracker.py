f98d88cfce5d9ecbc2a7659f6f0147e9da60af6d
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-27 16:00:45 +0200

    Added the Face Mesh

--- FaceTracker.py
+++ FaceTracker.py
@@ -7,8 +7,19 @@ cap = cv2.VideoCapture(0)
 pTime = 0 
 cTime = 0
 
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh()
+
 while True:
     success, img = cap.read()
+    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+    
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS)
     
     cTime = time.time()
     fps = 1/ (cTime - pTime)
